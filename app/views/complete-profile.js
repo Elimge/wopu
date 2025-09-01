a52fc45687db905b25677111e1c322647d746f3c
Miguel Canedo <elimge@outlook.com>
2025-09-01 12:19:38 -0500

    Update complete-profile.js fetch URL

--- app/views/complete-profile.js
+++ app/views/complete-profile.js
@@ -25,7 +25,7 @@ if (completeProfileForm) {
         }
 
         try {
-            const response = await fetch('http://localhost:3000/api/profile', {
+            const response = await fetch('https://wopu-api.onrender.com/api/profile', {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
