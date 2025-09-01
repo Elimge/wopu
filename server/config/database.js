3d7dd99db1d995d3d4668ef7a9a4830734516af2
Miguel Canedo <elimge@outlook.com>
2025-09-01 11:52:20 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -14,6 +14,7 @@ const mysql = require('mysql2');
  */
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -21,5 +22,6 @@ const connection = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
+
 
 module.exports = connection.promise();
