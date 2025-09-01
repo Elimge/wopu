ad0774c1682e3153f5506da056a2ab6b24597d43
Miguel Canedo <elimge@outlook.com>
2025-09-01 12:18:38 -0500

    Update admin.js fetch URL

--- app/views/admin.js
+++ app/views/admin.js
@@ -47,7 +47,7 @@ async function fetchAndRenderUsers() {
     }
 
     try {
-        const response = await fetch('http://localhost:3000/api/users', {
+        const response = await fetch('https://wopu-api.onrender.com/api/users', {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
